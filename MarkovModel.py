dbb8b40f181bedb52680a1539aef9a2b389c5804
Tyson Trauger <traugertyson@gmail.com>
2017-04-01 15:27:00 -0500

    Attempted to OOP MarkovModel

--- MarkovModel.py
+++ MarkovModel.py
@@ -4,33 +4,42 @@ from pyspark import SparkContext, SparkConf
 conf = SparkConf().setAppName("Markov Model")
 sc = SparkContext(conf=conf)
 
-lines = sc.textFile("books/54445-0.txt")
-lines = lines.map(lambda x: x.lower())
-tokens = lines.flatMap(lambda x: word_tokenize(x)) 
+class CreateDict(object): 
+    def __init__(self, bookArray):
+        self._bookArray = bookArray
+        self._word_map = {}
 
-word_map = {}
+    def createDict():
+        for i in _bookArray:
+            createDictForBook(i)
+        normalizeDict()
+        return self._word_map
 
-tok = tokens.collect()
-for i in range(len(tok)-1):
-	w1 = tok[i]
-	w2 = tok[i+1]
-	if w1 in word_map:
-		if w2 in word_map[w1]:
-			word_map[w1][w2] += 1
-		else:
-			word_map[w1][w2] = 1
-	else:
-		word_map[w1] = {w2: 1}
-	
+    def createDictForBook(book):
+        lines = sc.textFile(book)
+        lines = lines.map(lambda x: x.lower())
+        tokens = lines.flatMap(lambda x: word_tokenize(x)) 
 
-for i in word_map:
-	print(i + " ->" +  str(word_map[i]))
+        tok = tokens.collect()
+        for i in range(len(tok)-1):
+            w1 = tok[i]
+                w2 = tok[i+1]
+                if w1 in self._word_map:
+                    if w2 in self._word_map[w1]:
+                        self._word_map[w1][w2] += 1
+                    else:
+                        self._word_map[w1][w2] = 1
+                else:
+                    self._word_map[w1] = {w2: 1}
 
-# normalization of the data
-for i in matrix:
-	s = sum(i)
-	if(s != 0):
-		for k in range(len(i)):
-			i[k] /= s
+
+    def normalizeDict():
+        # normalization of the data
+        for i in self._word_map:
+            s = sum(self._word_map[i].values())
+            if(s != 0):
+                for k in self._word_map[i]:
+                    self._word_map[i][k] /= s
+
 
 
