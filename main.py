9eef70cc3bca94b9aaf4231f01db4bcda7086b15
Tyson Trauger <traugertyson@gmail.com>
2017-04-01 14:51:05 -0500

    Filled in the outline

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import string
+from .MarkovChain import MarkovChain
+from .MarkovModel import update_map
 """The main driver for creating the markov chain and the sentences"""
 
 #enter your books into this array
 bookArray = []
+
 #enter number of sentences here
 numSentences = 5
+
 #enter word you would like to start with
 startWord = 'A'
-#call Teja's code here
 
+stateDict = update_map(bookAarray)
 
-markovChain = MarkovChain(stateDict, startState)
-prev = ''
-while numSentences > 0:
-    currentState = markovChain.getState()
-    if currentState in string.punctuation:
-        numSentences-=1
-    print(markovChain.getState())
-    markovChain.nextState()
+if startWord not in stateDict:
+    print("The word you entered does not appear in any of the texts!")
+else:
+    markovChain = MarkovChain(stateDict, startState)
+    while numSentences > 0:
+        currentState = markovChain.getState()
+        if currentState in string.punctuation:
+            numSentences-=1
+        print(markovChain.getState())
+        markovChain.nextState()
 
 
 
